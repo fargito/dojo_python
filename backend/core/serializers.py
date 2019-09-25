54dc56f4b24b8be44fee176859947ebf31d87e4f
fargito <fr.farge96@gmail.com>
2019-09-25 20:19:41 +0200

    add score in user serializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,10 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from .models import User
 
 
 class UserSerializer(ModelSerializer):
+    score = SerializerMethodField()
+
+    def get_score(self, obj):
+        return 0
+
 
     class Meta:
         model = User
-        fields = ('username', )
+        fields = ('username', 'score')
