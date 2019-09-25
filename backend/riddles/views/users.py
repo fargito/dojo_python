4fa860c539c615d7ba1efba76545f7446795b22c
fargito <fr.farge96@gmail.com>
2019-09-25 20:14:19 +0200

    add users serializer

--- backend/riddles/views/users.py
+++ backend/riddles/views/users.py
@@ -1,6 +1,11 @@
+from core.models import User
+from core.serializers import UserSerializer
 from django.http import JsonResponse
 
 
 def get_users(request):
     """this endpoint returns the list of users and their current score"""
-    return JsonResponse({"users": []})
+    users = User.objects.filter(is_staff=False)
+    serializer = UserSerializer(users, many=True)
+
+    return JsonResponse({"users": serializer.data})
