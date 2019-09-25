cfa99a5caf54bdde64485db4a70a7959f0568fd0
fargito <fr.farge96@gmail.com>
2019-09-25 20:35:29 +0200

    add level to user models

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -4,12 +4,7 @@ from .models import User
 
 
 class UserSerializer(ModelSerializer):
-    score = SerializerMethodField()
-
-    def get_score(self, obj):
-        return 0
-
 
     class Meta:
         model = User
-        fields = ('username', 'score')
+        fields = ('username', 'level')
