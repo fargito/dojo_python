395a1fa87a84969071573d979886811074680299
jordanl <jordanl@theodo.fr>
2019-10-01 11:44:03 +0200

    Add polling to fetch users every second

--- frontend/src/redux/Users/sagas.ts
+++ frontend/src/redux/Users/sagas.ts
@@ -4,22 +4,28 @@ import { makeGetRequest } from 'services/networking/request';
 
 import { fetchUsers } from './actions';
 
+function delay(duration: number) {
+  const promise = new Promise(resolve => {
+    setTimeout(() => resolve(true), duration);
+  });
+  return promise;
+}
+
 // worker Saga: will be fired on USER_FETCH_REQUEST actions
 export function* fetchUserSaga(action: ActionType<typeof fetchUsers.request>) {
   const endpoint = `http://localhost:8000/api/users`;
   try {
     const response = yield call(makeGetRequest, endpoint);
     yield put(fetchUsers.success({ users: response.users }));
+
+    // wait for 1 second before the next polling
+    yield call(delay, 1000);
+    yield put(fetchUsers.request({}));
   } catch (error) {
     yield put(fetchUsers.failure({ errorMessage: error.message }));
   }
 }
 
-/*
-  Behavior similar to redux-thunk
-  Starts fetchUser on each dispatched `USER_FETCH_REQUEST` action.
-  Allows concurrent fetches of user.
-*/
 export default function* avatarSagas() {
   yield takeEvery(getType(fetchUsers.request), fetchUserSaga);
 }
