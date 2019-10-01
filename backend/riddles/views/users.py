059c1f192b33bde95c59265889a3c06711c14484
jordanl <jordanl@theodo.fr>
2019-10-01 12:11:19 +0200

    Sort users by level

--- backend/riddles/views/users.py
+++ backend/riddles/views/users.py
@@ -4,10 +4,12 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_users(request):
-    """this endpoint returns the list of users and their current score"""
+    """this endpoint returns the list of users and their current level"""
     users = User.objects.filter(is_staff=False)
     serializer = UserSerializer(users, many=True)
 
-    return Response({"users": serializer.data})
+    sorted_users_by_level = sorted(serializer.data, key=lambda user: -user["level"])
+
+    return Response({"users": sorted_users_by_level})
